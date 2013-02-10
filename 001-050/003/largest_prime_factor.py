72399825f88a3457079b866b77510792d9a2aabd
Aaylor <loloruna@orange.fr>
2013-02-10 10:43:01 +0100

    improve

--- 001-050/003/largest_prime_factor.py
+++ 001-050/003/largest_prime_factor.py
@@ -1,16 +1,14 @@
 #!/dev/bin/python
 
 def largest_prime_number(number):
-    divisor = 2
-    largest = 0
+    divisor = 1
 
     while number > 1:
-        while number % divisor == 0:
-            largest = divisor
-            number /= divisor
         divisor += 1
+        while number % divisor == 0:
+            number /= divisor
 
-    return largest
+    return divisor
 
 if __name__ == "__main__":
     print("Answer : {0}".format(largest_prime_number(600851475143)))
